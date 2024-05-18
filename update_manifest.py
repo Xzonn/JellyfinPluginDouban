bc0f441383e36ba0855d5f0dbcbafdcf0138c3f1
Xzonn <Xzonn@outlook.com>
2024-05-18 22:11:37 +0800

    Update manifest

--- update_manifest.py
+++ update_manifest.py
@@ -2,38 +2,48 @@
 # -*- coding: UTF-8 -*-
 
 import datetime
+import json
+import os
+import re
+import shutil
+
 from hashlib import md5
-import json, os, sys
 
-os.chdir(os.path.dirname(__file__))
+FILE_NAME_PATTERN = re.compile(r"Jellyfin.Plugin.Douban.(\d+).(\d+).(\d+).(\d+).zip")
 
-with open("manifest.json", "r", -1, "utf8") as reader:
+basic_path = os.path.dirname(os.path.abspath(__file__))
+
+with open(f"{basic_path}/manifest.json", "r", -1, "utf8") as reader:
   data = json.load(reader)
 
 versions: list[dict[str, str]] = data[0]["versions"]
-new_version = os.environ["XZ_VERSION"] or sys.argv[1]
 
-if not new_version:
-  raise ValueError("version is empty")
+for dotnet_version in [
+  "net6.0",
+  "net8.0",
+]:
+  path = f"{basic_path}/temp/build-{dotnet_version}"
+  for file_name in os.listdir(path):
+    new_version = FILE_NAME_PATTERN.search(file_name)
+    if not new_version:
+      continue
 
-for version in versions:
-  if version["version"] == f"{new_version}.0":
-    raise ValueError("version is existed")
+    major, minor, patch, revision = new_version.groups()
 
-major, minor, patch = new_version.split(".")
+    with open(f"{path}/{file_name}", "rb") as reader:
+      md5sum = md5(reader.read()).hexdigest()
 
-with open(f"dist/Jellyfin.Plugin.Douban.{new_version}.0.zip", "rb") as reader:
-  md5sum = md5(reader.read()).hexdigest()
+    versions.append({
+      "checksum": md5sum.upper(),
+      "changelog": f"https://xzonn.top/posts/Jellyfin-Plugin-Douban.html#%E6%9B%B4%E6%96%B0%E6%97%A5%E5%BF%97",
+      "targetAbi": f"10.{int(major) + 7}.0.0",
+      "sourceUrl": f"https://xzonn.top/JellyfinPluginDouban/dist/{file_name}",
+      "timestamp": datetime.datetime.now(tz=datetime.timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z"),
+      "version": f"{major}.{minor}.{patch}.{revision}",
+    })
 
-versions.append({
-  "checksum": md5sum.upper(),
-  "changelog": f"https://xzonn.top/posts/Jellyfin-Plugin-Douban.html#%E6%9B%B4%E6%96%B0%E6%97%A5%E5%BF%97",
-  "targetAbi": f"10.{int(major) + 7}.0.0",
-  "sourceUrl": f"https://xzonn.top/JellyfinPluginDouban/dist/Jellyfin.Plugin.Douban.{new_version}.0.zip",
-  "timestamp": datetime.datetime.now(tz=datetime.timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z"),
-  "version": f"{new_version}.0",
-})
+    shutil.move(f"{path}/{file_name}", f"{basic_path}/dist/{file_name}")
 
-with open("manifest.json", "w", -1, "utf8") as writer:
+with open(f"{basic_path}/manifest.json", "w", -1, "utf8") as writer:
   json.dump(data, writer, indent=2, ensure_ascii=False)
   writer.write("\n")
